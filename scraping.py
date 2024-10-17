68dea560fb449c33993199d38a6c45ac233728a3
AkshitaKhandelwal21 <khandelwalakshita041@gmail.com>
2024-10-17 10:54:14 +0530

    Adding function to get posts data

--- scraping.py
+++ scraping.py
@@ -1,10 +1,42 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
-
 from bs4 import BeautifulSoup
 import time
 import re
+
+
+# FUNCTION TO GET POSTS DATA
+def get_post_data(driver, post_url, max_retries):
+    for attempt in range(max_retries):
+        try:
+            driver.get(post_url)
+            wait = WebDriverWait(driver, 20)
+            wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'article')))
+
+            post_soup = BeautifulSoup(driver.page_source, 'html.parser')
+            caption_container = post_soup.find('div', {'class': '_a9zs'})
+
+            if caption_container:
+                h1_element = caption_container.find('h1')
+                if h1_element:
+                    caption_text = h1_element.get_text(strip=True, separator=' ')
+                else:
+                    caption_text = "Caption structure not recognized"
+            else:
+                caption_text = "No caption"
+
+            hashtags = [tag.get_text() for tag in post_soup.find_all('a') if tag.get_text().startswith('#')]
+            return {'caption': caption_text, 'hashtags': hashtags}
+
+        except (TimeoutException, StaleElementReferenceException) as e:
+            if attempt < max_retries - 1:
+                print(f"Attempt {attempt + 1} failed. Retrying...")
+                time.sleep(5)
+            else:
+                print(f"Error extracting caption after {max_retries} attempts: {e}")
+                return {'caption': "Error extracting caption", 'hashtags': []}
+
 
 
 #MAIN
