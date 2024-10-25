7c6091582588970329cda356e9100d43a76cfb41
AkshitaKhandelwal21 <khandelwalakshita041@gmail.com>
2024-10-25 22:55:08 +0530

    Added imports

--- scraping.py
+++ scraping.py
@@ -1,6 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException, StaleElementReferenceException
 from bs4 import BeautifulSoup
 import time
 import re
